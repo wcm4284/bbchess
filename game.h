ebf55e91205f92f218c131a38e8ae3707afe5cc7
unknown <willcmatherne@gmail.com>
2024-02-19 15:36:12 -0600

    creating game.h

---
+++ game.h
@@ -0,0 +1,58 @@
+#include <map>
+
+// board squares
+enum {
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1, no_sq
+};
+
+// colors
+enum { white, black, both };
+
+// bishop and rook
+enum { rook, bishop };
+
+// castling flags
+enum { wk = 1, wq = 2, bk = 4, bq = 8 };
+
+//encode pieces
+enum { P, N, B, R, Q, K, p, n, b, r, q, k };
+
+const char* square_to_coord[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1" 
+};
+
+// ASCII pieces
+const char ASCII_pieces[13] = "PNBRQKpnbrqk";
+
+// unicode pieces
+const char* unicode_pieces[13] = {"♟", "♞", "♝", "♜", "♛", "♚", "♙", "♘", "♗", "♖", "♕", "♔"};
+
+// ascii to unicode converter
+std::map<char, int> char_pieces = {
+    {'P', P},
+    {'N', N},
+    {'B', B},
+    {'R', R},
+    {'Q', Q},
+    {'K', K},
+    {'p', p},
+    {'b', b},
+    {'n', n},
+    {'r', r},
+    {'q', q},
+    {'k', k}
+};
