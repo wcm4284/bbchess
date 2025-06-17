a1584806232a53faf255d690ac859e3c226e11d3
Will Matherne <willcmatherne@gmail.com>
2025-06-17 14:45:08 -0500

    outline for move gen

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -2,16 +2,68 @@
 
 namespace Engine {
 
+template <PieceType pt>
+ExtMove* generate_moves(Bitboard target, const Position& pos, ExtMove* list) {
+
+	return list;
+}
+template <>
+ExtMove* generate_moves<PAWN>(Bitboard target, const Position& pos, ExtMove* list) {
+	return list;
+}
+
+template <>
+ExtMove* generate_moves<KNIGHT>(Bitboard target, const Position& pos, ExtMove* list) {
+	return list;
+}
+
+template <>
+ExtMove* generate_moves<BISHOP>(Bitboard target, const Position& pos, ExtMove* list) {
+	return list;
+}
+
+template <>
+ExtMove* generate_moves<ROOK>(Bitboard target, const Position& pos, ExtMove* list) {
+	return list;
+}
+
+template <>
+ExtMove* generate_moves<QUEEN>(Bitboard target, const Position& pos, ExtMove* list) {
+	return list;
+}
+
+template <>
+ExtMove* generate_moves<KING>(Bitboard target, const Position& pos, ExtMove* list) {
+	return list;
+}
+
+
+ExtMove* generate_all_moves(Bitboard target, const Position& pos, ExtMove* list) {
+	
+		
+	list = generate_moves<PAWN>(target, pos, list);
+	list = generate_moves<KNIGHT>(target, pos, list);
+	list = generate_moves<BISHOP>(target, pos, list);
+	list = generate_moves<ROOK>(target, pos, list);
+	list = generate_moves<QUEEN>(target, pos, list);
+	list = generate_moves<KING>(target, pos, list);
+
+	return list;
+}
+
 template <GenType T>
 ExtMove* generate(const Position& pos, ExtMove* list) {
 
-	// Bitboard checkers = pos.checkers(); TODO write checkers
 	Color us = pos.to_play();
-//	Bitboard target		 =   T == EVASIONS ? line_bb(ksq, checkers)
-//						   : T == NON_EVASIONS ? ~pos.pieces(us)
-//						   : T == CAPTURES ? pos.pieces(~us)
-//						   : ~pos.pieces();
-	
-	return nullptr;
+	Square ksq = pos.king_on(us);
+	Bitboard target		 =   T == EVASIONS ? line_bb(ksq, pos.checkers())
+						   : T == NON_EVASIONS ? ~pos.pieces(us)
+						   : T == CAPTURES ? pos.pieces(~us)
+						   : ~pos.pieces();
+	list = generate_all_moves(target, pos, list);
+	return list;
 }
+
+
+
 } // namespace Engine
