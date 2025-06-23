426a90351fcc87866e9d8fea4ff1b3a606dcd90e
Will Matherne <willcmatherne@gmail.com>
2025-06-23 16:47:01 -0500

    corrected typo

--- src/types.h
+++ src/types.h
@@ -131,7 +131,7 @@ const std::array<std::string, SQUARE_NB> printSquare = {
 	"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
 	"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
 	"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-	"a8", "b8", "c8", "d7", "e8", "f8", "g8", "h8"
+	"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
 };
 
 enum Direction {
