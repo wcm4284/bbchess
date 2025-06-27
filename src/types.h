a18fdbe16965e42a25450bc59e7dd325e3882cc3
Will Matherne <willcmatherne@gmail.com>
2025-06-27 15:49:34 -0500

    fixed eq sq print bug

--- src/types.h
+++ src/types.h
@@ -124,7 +124,7 @@ enum Square : int {
 };
 
 // defined twice, but wanted it here and too lazy to get rid of it elsewhere
-const std::array<std::string, SQUARE_NB> printSquare = {
+const std::array<std::string, 65> printSquare = {
 	"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
 	"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", 
 	"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
@@ -132,7 +132,8 @@ const std::array<std::string, SQUARE_NB> printSquare = {
 	"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
 	"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
 	"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-	"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
+	"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+	"-"
 };
 
 enum Direction {
