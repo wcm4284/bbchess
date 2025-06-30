eb1de769044749341a15434fe84c97b05d455d24
Will Matherne <willcmatherne@gmail.com>
2025-06-30 16:01:55 -0500

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "position.h"
 #include "movegen.h"
 #include "search.h"
+#include "evaluate.h"
 
 using namespace Engine;
 
@@ -10,35 +11,45 @@ int main() {
 
 	Bitboards::init();
 
-	std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	std::string fen = "rbnqknbr/pppppppp/1P6/8/8/8/PPPPPP1P/RBNQKNBR KQkq - 0 1";
 	
 	Position p;
-	p.init();
+	p.set_fen(fen);
+	std::cout << p << std::endl;
 /*
 	MoveList<LEGAL> ml = MoveList<LEGAL>(p);
-	p.do_move(&ml[3]);
-
+	p.do_move(&ml[2]);
 	std::cout << p << std::endl;
 
 	ml = MoveList<LEGAL>(p);
-	p.do_move(&ml[4]);
-
+	p.do_move(&ml[2]);
 	std::cout << p << std::endl;
 
 	ml = MoveList<LEGAL>(p);
-	p.do_move(&ml[0]);
-
+	p.do_move(&ml[19]);
 	std::cout << p << std::endl;
 
 	ml = MoveList<LEGAL>(p);
-	p.do_move(&ml[21]);
+	p.do_move(&ml[18]);
+	std::cout << p << std::endl;
 
+	ml = MoveList<LEGAL>(p);
+	p.do_move(&ml[28]);
+	std::cout << p << std::endl;
+
+	ml = MoveList<LEGAL>(p);
+	p.do_move(&ml[20]);
 	std::cout << p << std::endl;
 */
-	int d = 5;
-	uint64_t nodes = perft(p.fen(), d);
-	std::cout << "\nDepth: " << d << ", Nodes: " << nodes << std::endl << std::endl;
+	int d = 0;
+	for (int i = 1; i <= d; ++i) {
+		uint64_t nodes = perft(p.fen(), i);
+		std::cout << "\nNodes searched: " << nodes << std::endl << std::endl;}
 
+	std::cout << MoveList<LEGAL>(p) << std::endl;
+	std::cout << MoveList<CAPTURES>(p) << std::endl;
+
+	std::cout << iterative_deepening(p, 6) << std::endl;
 
 	return 0;
 
