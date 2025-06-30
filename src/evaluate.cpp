da834f75038de9ecf3483f8cf9fe8895195277fc
Will Matherne <willcmatherne@gmail.com>
2025-06-30 16:44:24 -0500

    added pawn sq array

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -2,6 +2,22 @@
 
 namespace Engine {
 
+
+namespace {
+
+	Value pawnSquareValue[SQUARE_NB] = {
+		0, 0, 0, 0, 0, 0, 0, 0,
+		5, 5, 5, 0, 0, 5, 10, 10,
+		10, 5, 5, 15, 15, 5, 5, 10,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		10, 10, 10, 10, 10, 10, 10, 10,
+		75, 75, 75, 75, 75, 75, 75, 75,
+		150, 150, 150, 150, 150, 150, 150, 150,
+		0, 0, 0, 0, 0, 0, 0, 0
+	};
+
+}
+
 Value static_eval(const Position& pos) {
 	Value eval = 0;
 
