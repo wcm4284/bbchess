4789571f095c80653a623df1de045c923d479a99
Will Matherne <willcmatherne@gmail.com>
2025-07-03 16:26:57 -0500

    put constructor back

--- src/moveorder.h
+++ src/moveorder.h
@@ -1,23 +1,21 @@
 #pragma once
 
 #include "movegen.h"
-#include "position.h"
 
 namespace Engine {
+
+class Position;
 
 template <GenType T>
 class MoveOrder {
 
 	public:
-		MoveOrder(const Position& p) : ptr(new MoveList<T>(p)) {
-			first = ptr->begin();
-			last = ptr->end();
-		}
+		MoveOrder(const Position& p) : ptr(new MoveList<T>(p)), first(ptr->begin()), last(ptr->end()) {}
 
-		~MoveOrder() {
-			delete ptr;}
+		~MoveOrder() { delete ptr; }
 
 		inline Move* next_move() { return first == last ? nullptr : first++; }
+		size_t size() const { return ptr->size(); }
 
 
 	private:
