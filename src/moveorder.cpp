6798a21a216f4bf1f5c48e01d21dc846d8683c67
Will Matherne <willcmatherne@gmail.com>
2025-07-07 12:02:18 -0500

    implemented move sorting, sped up search greatly

--- src/moveorder.cpp
+++ src/moveorder.cpp
@@ -1,7 +1,25 @@
+#include <algorithm>
+#include <functional>
+
 #include "moveorder.h"
 #include "position.h"
 
 namespace Engine {
 
+void rate_moves(const Position& p, ExtMove *f, ExtMove *l) {
+
+	while ( f != l ) {
+		f->value = p.pt_on(f->to_sq()) * 100 + PIECE_TYPE_NB - p.pt_on(f->from_sq());
+		++f;
+	}
+
+	return;
+}
 	
+void sort(const Position& p, ExtMove *f, ExtMove *l) {
+	rate_moves(p, f, l);
+	std::sort(f, l, std::greater<>());
+
+	return;
+}
 } // namespace
--- src/moveorder.h
+++ src/moveorder.h
@@ -6,12 +6,13 @@ namespace Engine {
 
 class Position;
 
+void sort(const Position&, ExtMove*, ExtMove*);
+
 template <GenType T>
 class MoveOrder {
 
 	public:
-		MoveOrder(const Position& p) : ptr(new MoveList<T>(p)), first(ptr->begin()), last(ptr->end()) {}
-
+		MoveOrder(const Position& p) : ptr(new MoveList<T>(p)), first(ptr->begin()), last(ptr->end()) { sort(p, first, last); }
 		~MoveOrder() { delete ptr; }
 
 		inline Move* next_move() { return first == last ? nullptr : first++; }
@@ -19,6 +20,7 @@ class MoveOrder {
 
 
 	private:
+
 		MoveList<T>* ptr;
 		ExtMove *first, *last;
 
