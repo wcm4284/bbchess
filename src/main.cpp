e0b0bf12be12a0772876c9bdd91fac02dd5e34c7
Will Matherne <willcmatherne@gmail.com>
2025-07-11 14:41:59 -0500

    nothing of importance

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,21 @@ int main() {
 
 
 	std::string fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
+	std::string fen2 = "k7/7Q/1K6/8/8/8/8/8 w - - 0 10";
 	
 	Position p;
-	p.init();
-//	p.set_fen(fen);
-	std::cout << p << std::endl;
-	MoveOrder<LEGAL> mo(p);
-	
+//	p.init();
+	p.set_fen(fen2);
+//	std::cout << p << std::endl;
 
-	for (int i = 1; i <= 7; ++i) {
+//	std::cout << Eval::pretty(p) << std::endl;
+
+	//MoveOrder<LEGAL> mo(p);
+	
+	
+//	Search::perft(fen2, 2);
+
+	for (int i = 1; i <= 10; ++i) {
 		auto start = std::chrono::high_resolution_clock::now();
 
 		Search::iterative_deepening(p, i);
@@ -33,10 +39,9 @@ int main() {
 
 		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-		std::cout << "Time taken " << duration.count() << " ms" << std::endl;
+		std::cout << "Time taken " << duration.count() << " ms\n\n";
 
-		std::cout << "\n\n\n";}
-
+	}
 	return 0;
 
 }
