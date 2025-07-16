729d63dc4feb87328631d575b555eb5b3b6b329f
Will Matherne <willcmatherne@gmail.com>
2025-07-16 16:35:56 -0500

    feels pretty set up to start working soon

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,6 +2,16 @@
 
 namespace Engine {
 
-Engine::Engine(int nThreads) {}
+Engine::Engine(int nThreads) : threads(nThreads), pos() {}
+
+void Engine::perft(int depthLimit) {
+
+	std::cout << "perft with depthLimit " << depthLimit << std::endl;
+}
+
+void Engine::go(int depthLimit) {
+	std::cout << "go with depthLimit " << depthLimit << std::endl;
+}
+
 
 }
--- src/engine.h
+++ src/engine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "thread.h"
+#include "position.h"
 
 namespace Engine {
 
@@ -8,11 +9,18 @@ class Engine {
 
 	public:
 		Engine(int nThreads);
+		
+		// this function isn't very versatile, and will likely lead to a seg fault
+		// if the fen string isn't formatted correctly
+		inline void set_position(std::string fen) { pos.set(fen); }
+		void perft(int = MAX_PLY);
+		void go(int = MAX_PLY);
 
 
 	private:
 		
 		ThreadPool threads;
+		Position pos;
 		
 
 };
