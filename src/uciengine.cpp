e341c7e86d402d86cf1778eed69805e4dc55b548
Will Matherne <willcmatherne@gmail.com>
2025-07-16 16:33:26 -0500

    changed loop semantic
    i was using exit() before, but this led to memory problems. this fixes

--- src/uciengine.cpp
+++ src/uciengine.cpp
@@ -5,63 +5,87 @@
 
 namespace Engine {
 
-UCIEngine::UCIEngine() : engine(1) {}
+
+namespace {
+
+	std::string startFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+}
+
+UCIEngine::UCIEngine() : engine(10) {}
 
 void UCIEngine::loop() {
 
 	std::string token, cmd;
 
-	while (true) {
+	do {
 
-		std::getline(std::cin, cmd); 
+		std::getline(std::cin, cmd);
 
 		std::istringstream is(cmd); 
 
 		is >> token;
 
-		if (token == "quit" || token == "exit" || token == "stop")
-			exit(1);
+		if (token == "quit" || token == "exit") {}
 
 
-		if (token == "position") {
+		else if (token == "position") {
 			position(is);
 		} else if (token == "go") {
 			go(is);
 		} else {
-			std::cout << "Unrecognized token " << token << ". Please try again.\n";
+			std::cout << "Unrecognized token '" << token << "'. Please try again.\n";
 		}
 
 
 
-	}
+	} while (token != "quit" && token != "exit");
 }
 
 void UCIEngine::position(std::istringstream& is) {
 
-	std::cout << "parsing position\n";
-
 	std::string token;
 	is >> token;
 
+	std::string fen;
+
 	if (token == "startpos") {
-		//engine.set_position();
+		fen = startFEN;
 		is >> token; // get rid of "moves" that may follow
 	} else if (token == "fen") {
-		std::string fen;
-
-		while (is >> token) {
+		while (is >> token && token != "moves") {
 			fen += token + " ";
 
 		}
-
-		std::cout << fen;
-
 	}
+	
+	
+	std::cout << fen << "\n";
+
+	engine.set_position(fen);
 }
 
 void UCIEngine::go(std::istringstream& is) {
-	std::cout << "parsing go\n";
+
+	std::string token;
+	if (!(is >> token))
+		engine.go();
+
+	if (token == "perft") {
+		
+		if (!(is >> token))
+			engine.perft();
+		else
+			engine.perft(std::stoi(token));
+
+	} else if (token == "depth") {
+		
+		is >> token;
+		engine.go(std::stoi(token));
+
+	}
+
 }
 
 
-}
+} // namespace Engine
